69122c9164bf4b1767aa94fb2a53b2500896aadd
yooonwodyd <dbswodyd00@naver.com>
2024-09-24 01:00:12 +0900

    feat:[#22]- add UserRepository
    JPA유저레포지토리 구현.

---
+++ src/main/java/com/helpmeCookies/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.helpmeCookies.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helpmeCookies.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
