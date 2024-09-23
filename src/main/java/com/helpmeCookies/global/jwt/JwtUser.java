75bbfea33c201a90f92278f5785c843c8c65f86a
yooonwodyd <dbswodyd00@naver.com>
2024-09-24 00:53:56 +0900

    feat:[#22]-Add JwtProvider
    JwtProvider 구현. reissueAccessToken에 TODO가 존재한다.

---
+++ src/main/java/com/helpmeCookies/global/jwt/JwtUser.java
@@ -0,0 +1,57 @@
+package com.helpmeCookies.global.jwt;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class JwtUser implements UserDetails {
+	private Long id;
+
+	public static JwtUser of(Long id) {
+		return JwtUser.builder()
+			.id(id)
+			.build();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+}
