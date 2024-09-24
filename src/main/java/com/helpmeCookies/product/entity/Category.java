699df05b47a87881d409340750c69c91bd1a102d
bokyeong <wntjdals0412@gmail.com>
2024-09-25 00:55:12 +0900

    feat: [#26] 해시태그, 카테고리 ENUM 항목 추가 필요시 추후 추가 예정

--- src/main/java/com/helpmeCookies/product/entity/Category.java
+++ src/main/java/com/helpmeCookies/product/entity/Category.java
@@ -1,5 +1,20 @@
 package com.helpmeCookies.product.entity;
 
 public enum Category {
-	PICTURE;
+	PICTURE("사진"),
+	ORIENTAL("동양화"),
+	WESTERN("서양화"),
+	PIECE("조각화"),
+	CERAMIC("도예/공예"),
+	NEWMEDIA("뉴미디어"),
+	DESIGN("디자인"),
+	DRAWING("드로잉/판화"),
+	CALLIGRAPHY("서예/캘리그라피")
+	;
+
+	private String name;
+
+	Category(String name) {
+		this.name = name;
+	}
 }
