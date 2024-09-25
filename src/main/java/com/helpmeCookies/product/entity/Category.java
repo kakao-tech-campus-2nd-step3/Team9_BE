aa2dd0b53bb6fa739c77aac808f14afb41747686
bokyeong <wntjdals0412@gmail.com>
2024-09-25 10:32:32 +0900

    feat: [#25] String -> ENUM 변환코드

--- src/main/java/com/helpmeCookies/product/entity/Category.java
+++ src/main/java/com/helpmeCookies/product/entity/Category.java
@@ -1,5 +1,8 @@
 package com.helpmeCookies.product.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Category {
 	PICTURE("사진"),
 	ORIENTAL("동양화"),
@@ -12,9 +15,29 @@ public enum Category {
 	CALLIGRAPHY("서예/캘리그라피")
 	;
 
-	private String name;
+	private final String name;
+	private static final Map<String, Category> nameToCategoryMap = new HashMap<>();
+
+	static {
+		for (Category category : Category.values()) {
+			nameToCategoryMap.put(category.getName(), category);
+		}
+	}
 
 	Category(String name) {
 		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static Category fromString(String name) {
+		System.out.println(name);
+		Category category = nameToCategoryMap.get(name);
+		if (category == null) {
+			throw new IllegalArgumentException(name + "에 해당하는 카테고리가 없습니다.");
+		}
+		return category;
 	}
 }
