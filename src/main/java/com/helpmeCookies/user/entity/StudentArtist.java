6f7a3ab40dabd283d58de642bbc9a252486a09f9
yooonwodyd <dbswodyd00@naver.com>
2024-09-27 18:37:05 +0900

    feat:[#24]- Add BusinessArtist
    userId 추가.

--- src/main/java/com/helpmeCookies/user/entity/StudentArtist.java
+++ src/main/java/com/helpmeCookies/user/entity/StudentArtist.java
@@ -6,13 +6,26 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
+@Builder
 public class StudentArtist {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
+	private Long userId;
 
 	@Column(nullable = false)
 	private String schoolEmail;
