e36a9c6735fa341f7e8b1dba414d5e103b9ac3b9
sim-mer <gyu2781@gmail.com>
2024-10-03 03:55:06 +0900

    #27 add: JpaAuditing을 이용한 BaseTimeEntity 작성

--- src/main/java/com/helpmeCookies/Step3Application.java
+++ src/main/java/com/helpmeCookies/Step3Application.java
@@ -2,8 +2,10 @@ package com.helpmeCookies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Step3Application {
 
 	public static void main(String[] args) {
