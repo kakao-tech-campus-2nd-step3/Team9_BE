c4129bd0e3c144663a3ba601a42b75a9abc88db9
yooonwodyd <dbswodyd00@naver.com>
2024-10-04 22:28:52 +0900

    feat:[#24]- Add EntityListeners
    자동 날짜 추가를 위해 JPAConfig와 EntityListeners 추가

--- src/main/java/com/helpmeCookies/user/entity/User.java
+++ src/main/java/com/helpmeCookies/user/entity/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.helpmeCookies.product.entity.HashTag;
@@ -12,6 +13,7 @@ import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +32,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users") // 예약어 회피
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
