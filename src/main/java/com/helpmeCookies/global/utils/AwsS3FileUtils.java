7012a460774291108032a51a88e184316acac46b
bokyeong <wntjdals0412@gmail.com>
2024-10-12 02:20:09 +0900

    fix: [#30] S3 다중 파일 업로드 구현

---
+++ src/main/java/com/helpmeCookies/global/utils/AwsS3FileUtils.java
@@ -0,0 +1,71 @@
+package com.helpmeCookies.global.utils;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AwsS3FileUtils {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    //다중파일 업로드후 url 반환
+    public List<String> uploadMultiImages(List<MultipartFile> multipartFiles) {
+        List<String> fileList = new ArrayList<>();
+
+        multipartFiles.forEach(file -> {
+            String fileName = createFileName(file.getOriginalFilename()); //파일 이름 난수화
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try (InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch (IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드 실패" + fileName);
+            }
+
+            fileList.add(fileName);
+        });
+
+        return fileList;
+    }
+
+    public String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    //TODO error handler 필요
+    public String getFileExtension(String fileName) {
+        try {
+            String extension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
+            //이미지 파일 확장자 목록
+            List<String> allowedExtensions = Arrays.asList(".jpg", ".jpeg", ".png");
+
+            if (!allowedExtensions.contains(extension)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미지 파일만 업로드가 가능합니다. 지원되지 않는 형식의 파일" + fileName);
+            }
+            return extension;
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"잘못된 형식의 파일" + fileName + "입니다.");
+        }
+    }
+}
