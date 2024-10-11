fd0032a72a7913a8c3f1e02077caf9b7b7c339a5
bokyeong <wntjdals0412@gmail.com>
2024-10-12 02:19:28 +0900

    fix: [#30] S3Config 파일 Bean 어노테이션 설정

--- src/main/java/com/helpmeCookies/global/config/S3Config.java
+++ src/main/java/com/helpmeCookies/global/config/S3Config.java
@@ -6,6 +6,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,6 +18,7 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Bean
     public AmazonS3 amazonS3() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey,secretKey);
 
