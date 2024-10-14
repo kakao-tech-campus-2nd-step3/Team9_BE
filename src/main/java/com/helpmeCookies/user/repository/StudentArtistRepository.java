b7411e5481030c756e2e0cf3f71c36ed392f1911
yooonwodyd <dbswodyd00@naver.com>
2024-10-14 16:19:56 +0900

    feat:[#43]- add ArtistService
    작가 서비스 로직 추가

--- src/main/java/com/helpmeCookies/user/repository/StudentArtistRepository.java
+++ src/main/java/com/helpmeCookies/user/repository/StudentArtistRepository.java
@@ -1,10 +1,14 @@
 package com.helpmeCookies.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.helpmeCookies.user.entity.ArtistInfo;
 import com.helpmeCookies.user.entity.StudentArtist;
 
 @Repository
 public interface StudentArtistRepository extends JpaRepository<StudentArtist, Long> {
+	Optional<StudentArtist> findByArtistInfo(ArtistInfo artistInfo);
 }
