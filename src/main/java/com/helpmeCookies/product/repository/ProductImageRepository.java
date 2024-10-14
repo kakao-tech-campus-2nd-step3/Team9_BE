49de4fb00299b3b1a0a6250d7563a1baebdfecd7
bokyeong <wntjdals0412@gmail.com>
2024-10-15 00:05:51 +0900

    feat: [#30] 상품 삭제시 관련 사진엔티티 삭제 로직 추가

--- src/main/java/com/helpmeCookies/product/repository/ProductImageRepository.java
+++ src/main/java/com/helpmeCookies/product/repository/ProductImageRepository.java
@@ -4,6 +4,10 @@ import com.helpmeCookies.product.entity.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage,Long> {
+    List<ProductImage> findAllByProductId(Long productId);
+    void deleteAllByProductId(Long productId);
 }
