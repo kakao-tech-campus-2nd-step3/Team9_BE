d25ba35904b0196176a62da735664aad566035aa
yooonwodyd <dbswodyd00@naver.com>
2024-10-14 16:17:58 +0900

    feat:[#43]- add Artist Dto
    작가관련 Dto 추가 작성

--- src/main/java/com/helpmeCookies/user/dto/ArtistInfoDto.java
+++ src/main/java/com/helpmeCookies/user/dto/ArtistInfoDto.java
@@ -1,11 +1,29 @@
 package com.helpmeCookies.user.dto;
 
+import com.helpmeCookies.user.entity.ArtistInfo;
+import com.helpmeCookies.user.entity.ArtistType;
+
+import lombok.Builder;
+
+@Builder
 public record ArtistInfoDto(
+	Long id,
+	Long userId,
+	ArtistType artistType,
+	String nickname,
 	Long totalFollowers,
 	Long totalLikes,
 	String about
 ) {
-	public static ArtistInfoDto of(Long totalFollowers, Long totalLikes, String about) {
-		return new ArtistInfoDto(totalFollowers, totalLikes, about);
+	public static ArtistInfoDto fromEntity(ArtistInfo artistInfo) {
+		return ArtistInfoDto.builder()
+			.id(artistInfo.getId())
+			.userId(artistInfo.getUserId())
+			.artistType(artistInfo.getArtistType())
+			.nickname(artistInfo.getNickname())
+			.totalFollowers(artistInfo.getTotalFollowers())
+			.totalLikes(artistInfo.getTotalLikes())
+			.about(artistInfo.getAbout())
+			.build();
 	}
 }
